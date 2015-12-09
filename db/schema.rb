bfa5eb5382727c19ec4260c2d6d2edd9e412a231
jyork779 <jonathanyork02@gmail.com>
2015-12-08 23:40:01 -0500

    Add name to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209031312) do
+ActiveRecord::Schema.define(version: 20151209041006) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "sender_id"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20151209031312) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
